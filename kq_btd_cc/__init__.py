fe7c6b8a8eac3b7ce0719047e9a5db211bd95bf3
LukeGSW <lukeguitarskywalker@gmail.com>
2025-08-19 23:40:35 -0700

    Update __init__.py

--- kq_btd_cc/__init__.py
+++ kq_btd_cc/__init__.py
@@ -1,1 +1,16 @@
-__all__ = ["style", "utils", "data_api", "core"]
+"""kq_btd_cc
+Pacchetto per la strategia Boosted Covered Call (mensile) con BTD.
+
+Contiene:
+- data_api: download e preparazione dati EODHD (OHLC aggiustati)
+- utils: funzioni di utilità per formattazione e plotting
+- style: configurazioni estetiche dei grafici
+- core: logica di simulazione e generazione figure
+"""
+
+from .style import STYLE_CONFIG
+from .core import esegui_analisi_completa
+
+__all__ = ["STYLE_CONFIG", "esegui_analisi_completa"]
+
+__version__ = "1.0.0"
